c16d57b26acd4d02a4b3032a5b1b130e1ad7b34e
sumitha <sumitha.kurien@gmail.com>
2023-06-15 15:49:29 +1000

    changes to main def

--- gen_harness/main.py
+++ gen_harness/main.py
@@ -6,12 +6,13 @@ org_name = os.getenv('HARNESS_ORG_NAME')
 account_id = os.getenv('HARNESS_ACCOUNT_IDENTIFIER')
 api_key = os.getenv('HARNESS_API_KEY')
 
-url = '{}{}/api/connectors?accountIdentifier={}'.format(base_url, org_name, account_id)
-print(url)
-json_file = 'resources/connector.json'
-harness_connector = HarnessConnector(url, json_file, api_key)
-result = harness_connector.create_connector()
-print(result)
+def main():
+  url = '{}{}/api/connectors?accountIdentifier={}'.format(base_url, org_name, account_id)
+  print(url)
+  json_file = 'resources/connector.json'
+  harness_connector = HarnessConnector(url, json_file, api_key)
+  result = harness_connector.create_connector()
+  print(result)
 
 if __name__ == '__main__':
     main()
